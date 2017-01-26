cb0f8cae8473b5b6de7af7b747bccdbf50dec44c
Anguchelvan <anguchelvan@gmail.com>
2017-01-26 09:42:05 +0530

    html client

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
 
-var index = require('./routes/index');
-var tasks = require('./routes/tasks');
+//var index = require('./routes/index');
+//var tasks = require('./routes/tasks');
 
 var port = 3000;
 //view engine
-app.set('views',path.join(__dirname + 'views'));
+app.set('views',path.join(__dirname , 'views'));
 app.set('view engine','ejs');
 app.engine('html',require('ejs').renderFile);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({encoded : false}));
 
-mongodb://<dbuser>:<dbpassword>@ds131119.mlab.com:31119/angu_mongolab_test
+app.use(express.static(path.join(__dirname ,'client' )));
+//mongodb://<dbuser>:<dbpassword>@ds131119.mlab.com:31119/angu_mongolab_test
 
 app.use('/',index);
 app.use('/api',tasks);
