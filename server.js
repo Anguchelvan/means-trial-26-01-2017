14631e3562d9c44639c14171068550ba7d5769d6
Anguchelvan <anguchelvan@gmail.com>
2017-01-26 09:50:28 +0530

    change all

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 //var index = require('./routes/index');
 //var tasks = require('./routes/tasks');
 
-var port = 3000;
+//var port = 3000;
 //view engine
-app.set('views',path.join(__dirname , 'views'));
-app.set('view engine','ejs');
-app.engine('html',require('ejs').renderFile);
+//app.set('views',path.join(__dirname , 'views'));
+//app.set('view engine','ejs');
+//app.engine('html',require('ejs').renderFile);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({encoded : false}));
@@ -18,8 +18,8 @@ app.use(bodyParser.urlencoded({encoded : false}));
 app.use(express.static(path.join(__dirname ,'client' )));
 //mongodb://<dbuser>:<dbpassword>@ds131119.mlab.com:31119/angu_mongolab_test
 
-app.use('/',index);
-app.use('/api',tasks);
+//app.use('/',index);
+//app.use('/api',tasks);
 //app.listen(port,function(){
 //
 //	console.log("connected to the port" + port);
